181d31cc888b342bc3d95b1f61e78082a85eefe0
yoonsoo.lee <yoonsoo.lee@bespinglobal.com>
2022-12-16 15:01:27 +0900

    재배포 6

--- codedeploy/start_server.sh
+++
@@ -1,4 +0,0 @@
-DEPLOY_PATH="/home/ec2-user/portal"
-DEPLOY_DIR="portal-source"
-
-pm2 start ${DEPLOY_PATH}/${DEPLOY_DIR}/src/index.js
--- codedeploy/stop_server.sh
+++
@@ -1,5 +0,0 @@
-DEPLOY_PATH="/home/ec2-user/portal"
-DEPLOY_DIR="portal-source"
-
-pm2 delete index
-rm -rf ${DEPLOY_PATH}/${DEPLOY_DIR}
